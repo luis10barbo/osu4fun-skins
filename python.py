a7002add10064551f74090375c616e7ca1e8a2b4
Luis Eduardo <luis10barbo2020@gmail.com>
2022-03-07 10:30:43 -0300

    Updated README.md formatting

--- python.py
+++ python.py
@@ -7,8 +7,9 @@ lines = text.split("\n")
 current_text = ""
 for i in range(len(lines)):
     if lines[i] == "#":
-        current_text = f"# {lines[i + 1]}\n{lines[i + 2]}\n"
+        
+        current_text = f"# {lines[i + 1]}\n{lines[i+2]}\n"
         new_text += current_text
     
-with open("teste.md", "w") as file:
+with open("README.md", "w") as file:
     file.write(new_text)
